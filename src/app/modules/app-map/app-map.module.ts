38264ce7292675a31cb0d4a362e2a9b57bbef2bd
Dmitry <you@example.com>
2018-04-23 20:45:10 +0700

    map sample

---
+++ src/app/modules/app-map/app-map.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapSampleComponent } from './pages/map-sample/map-sample.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MapSampleComponent]
+})
+export class AppMapModule { }
---
+++ src/app/modules/app-map/pages/map-sample/map-sample.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-sample',
+  templateUrl: './map-sample.component.html',
+  styleUrls: ['./map-sample.component.css']
+})
+export class MapSampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
